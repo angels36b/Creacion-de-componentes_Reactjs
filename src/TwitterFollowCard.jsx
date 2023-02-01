05a13dc5680acad1f1377d0283c16605cd157241
Angel <angel.36b@gmail.com>
2023-01-31 23:14:22 -0500

    TwitterFollowCard

---
+++ src/TwitterFollowCard.jsx
@@ -0,0 +1,35 @@
+// import react from "@vitejs/plugin-react-swc"
+
+import { useState } from "react"
+
+export function TwitterFollowCard ({userName, name, image, formatUserName}) {
+
+    const [isFollowing, setIsFollowing] = useState(false)
+
+   
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'   
+
+    const handleClick = () =>{
+        setIsFollowing(!isFollowing)
+    }
+
+return(
+    <article className='tw-followCard'>
+        <header className='tw-followCard-header'>
+            <img className='tw-followCard-avatar' 
+            alt="photo Angel" 
+            src={image}/>
+        <div className='tw-followCard-info'>
+            <strong>{name}</strong>
+            <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
+        </div>
+            </header>
+            <aside>
+                <button className={buttonClassName} onClick={handleClick}>
+                    {text}
+                </button>
+            </aside>
+    </article>
+)
+}
